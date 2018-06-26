57e34e159815ed57692fccd2cf24c9ff06a8a36b
Mig <mig@campbellhay.com>
2018-06-26 17:38:29 +0100

    Change defaults

--- public/js/main.js
+++ public/js/main.js
@@ -2,10 +2,19 @@ var is_touch_device = !!('ontouchstart' in window) || !!('msmaxtouchpoints' in w
 
 $(function() {
 
+  // Elements
+
+  var $window = $(window);
+  var html_body = $('html,body');
+
+  // Touch devices class
+
   if (is_touch_device) {
     // For being able to use `body.touch-device` in CSS
     $('body').addClass("touch-device");
   }
+
+  // Lazy load
 
   $('img.lazy').lazyload({
     data_attribute: 'src',
