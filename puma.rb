1c302811ad252de790f62854a0559a2ae6d792f6
Mickael Riga <mig@mypeplum.com>
2019-11-14 17:26:10 +0100

    Update puma config to have threads adapted to Heroku use

--- puma.rb
+++ puma.rb
@@ -1,5 +1,6 @@
+threads Integer(ENV['MIN_THREADS'] || 5), Integer(ENV['MAX_THREADS'] || 5)
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 preload_app!
-port ENV['PORT'] || 9292
+port ENV['PORT'] || 8080
 environment ENV['RACK_ENV'] || 'development'
 
